62b43b69adcc0e2e58cea8a96e8f78a540a6508b
GojkoMaric <gojko.maric@yahoo.com>
2018-08-07 18:39:26 +0200

    Zadatak 1

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <table border=1 align="center">
+        <thead>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Date of Birth</th>
+        </thead>
+        <tbody>
+          <tr v-for="animal in animals" :key="animal.id">
+            <td> {{animal.species}} </td>
+            <td> {{animal.name}} </td>
+            <td> {{animal.dateOfBirth}} </td>
+          </tr>
+        </tbody>
+      </table>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data() {
+    return {
+      proba: "lalalalalal",
+      animals: [
+        {species: "Horse", name: "Zelenko", dateOfBirth: "09/01/1995"},
+        {species: "Pig", name: "Gile", dateOfBirth: "20/10/2004"},
+        {species: "Ruster", name: "Zvonko", dateOfBirth: "20/07/2011"},
+        {species: "Mouse", name: "Gricko", dateOfBirth: "30/04/2015"},
+        {species: "Zebra", name: "Zaza", dateOfBirth: "22/11/2008"},
+
+      ]
+      
+    };
+  },
+}
+</script>
+
